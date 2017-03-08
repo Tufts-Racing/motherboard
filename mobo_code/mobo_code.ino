74648abaf54306b28c60f13ec22e8d9f838948b5
petermalinovsky <peter@malinovsky.net>
2017-03-08 14:10:48 -0500

    Code edit and added pins file

--- mobo_code/mobo_code.ino
+++ mobo_code/mobo_code.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include "pins.h"
 
 uint8_t rpm = 99;           //rpm
 uint8_t odo = 99;           //miles
@@ -13,14 +14,43 @@ uint8_t IMD_FLT = 0;
 uint8_t BMS_FLT = 0;
 uint8_t SEVCON_FLT = 0;
 uint8_t BRAKE_FLT = 0;
+uint8_t TMSM_FLT = 0;
+
+uint8_t START_INPUT = 0;
+uint8_t COCKPIT_SW = 0;
+
+uint8_t uglow = 0;
+
 
 const uint8_t WHEELADDR = 8;
+
 
 void setup() {
   Wire.begin(); // join i2c bus as master
 }
 
 void loop() {
+  
+  transmit();
+  readIn();
+
+
+  //
+  START_INPUT = digitalRead(START_INPUT_IN);
+  if (START_INPUT == HIGH)
+  {
+      digitalWrite(CONT_REQ_OUT, HIGH);
+      
+      Wire.requestFrom(WHEELADDR, 2);
+      dir = Wire.read();
+      uglow = Wire.read();
+      
+  }
+  
+  
+}
+
+void transmit() {
   Wire.beginTransmission(WHEELADDR); // transmit to device #8
   
   //Wire.write(rpm);
@@ -37,5 +67,19 @@ void loop() {
   Wire.write(SEVCON_FLT);
   Wire.write(BRAKE_FLT);
   
-  Wire.endTransmission();    // stop transmitting
+  Wire.endTransmission();    
+  //Stop transmitting
 }
+
+void readIn() {
+  
+  uint8_t IMD_FLT = digitalRead(IMD_FLT_IN);
+  uint8_t BMS_FLT = digitalRead(BMS_FLT_IN);
+  uint8_t SEVCON_FLT = digitalRead(IMD_FLT_IN);
+  uint8_t BRAKE_FLT = digitalRead(BOTS_IN);
+  uint8_t TMSM_FLT = digitalRead(TSMS_IN);
+  
+  uint8_t START_INPUT = digitalRead(START_INPUT_IN);
+  uint8_t COCKPIT_SW = digitalRead(COCKPIT_SW_IN);
+}
+
---
+++ mobo_code/pins.h
@@ -0,0 +1,25 @@
+#ifndef PINS_H
+#define PINS_H
+
+//
+#define RX_IN D0
+#define TX_OUT D1
+
+#define COCKPIT_SW_IN D2
+#define TSMS_IN D3
+#define BOTS_IN D4
+
+#define CONT_REQ_OUT D5
+
+//Faults
+#define IMD_FLT_IN D6
+#define BMS_FLT_IN D7
+
+//
+#define UGLOW_OUT D8
+#define RTDS_OUT D9
+
+//Start input
+#define START_INPUT_IN D10
+
+#endif
