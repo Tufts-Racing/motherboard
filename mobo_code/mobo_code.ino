29ee6cfe997eded49e626aff9468da005b1dbe1c
noelhwang <noelhwang@gmail.com>
2017-03-12 01:48:11 -0500

    Driver Reset and drive direction
    Added Cockpit switch and drive direction logic

--- mobo_code/mobo_code.ino
+++ mobo_code/mobo_code.ino
@@ -10,48 +10,66 @@ uint8_t spd = 10;           //speed in mph
 uint8_t LV_SOC = 5;         //Low Volatage State of Charge
 uint8_t HV_SOC = 7;         //Low Voltage State of Charge
 
-uint8_t IMD_FLT = 0;
-uint8_t BMS_FLT = 0;
-uint8_t SEVCON_FLT = 0;
-uint8_t BRAKE_FLT = 0;
-uint8_t TMSM_FLT = 0;
+uint8_t IMD_FLT = 0;        //IMD Fault state variable
+uint8_t BMS_FLT = 0;        //BMS Fault state variable
+uint8_t SEVCON_FLT = 0;     //Sevcon Fault
+uint8_t BRAKE_FLT = 0;      //BOTS Fault
+uint8_t TSMS_FLT = 0;       //TSMS Open
 
-uint8_t START_INPUT = 0;
-uint8_t COCKPIT_SW = 0;
+uint8_t COCKPIT_SW = 0;     //cockpit switch
 
 uint8_t uglow = 0;
-
+uint8_t car_started = 0;
 
 const uint8_t WHEELADDR = 8;
 
+void transmit();
+void readIn();
+void playRTDS(void);
 
 void setup() {
   Wire.begin(); // join i2c bus as master
+  pinMode(START_INPUT_IN, INPUT);
+  pinMode(CONT_REQ_OUT, OUTPUT);
+  pinMode(DRIVE_IN, INPUT_PULLUP); //active low
+  pinMode(REVERSE_IN, INPUT_PULLUP); //active low
+  pinMode(IMD_FLT_IN, INPUT);
+  pinMode(BMS_FLT_IN, INPUT);
+  pinMode(SEVCON_FLT_IN, INPUT);
+  pinMode(BOTS_IN, INPUT);
 }
 
 void loop() {
-  
-  transmit();
+
+
+
   readIn();
-
-
-  //
-  START_INPUT = digitalRead(START_INPUT_IN);
-  if (START_INPUT == HIGH)
+  transmit();
+  //check to see if car requested to start
+  if (digitalRead(START_INPUT_IN) && car_started == 0) //for case when cold start i.e. cockpit switch not tripped
   {
+      car_started = 1; //set flag that car has been started
+      digitalWrite(CONT_REQ_OUT, HIGH); //set contact request high to close AIRs
+      delay(6000); //wait for precharge
+      while(DRIVE_IN && REVERSE_IN){ // while no direction input, car is in neutral, wait for drive direction input. NOTE:DRIVE_IN and REVERSE_IN are active low
+      //spin until forward or reverese is selected
+      }
       digitalWrite(CONT_REQ_OUT, HIGH);
-      
-      Wire.requestFrom(WHEELADDR, 2);
-      dir = Wire.read();
-      uglow = Wire.read();
-      
+      delay(6000); //wait for precharge
+      playRTDS();
   }
-  
+  if(!COCKPIT_SW && BRAKE_FLT && car_started == 1){ //cockpit switch tripped by driver after car has been started
+    digitalWrite(CONT_REQ_OUT, LOW); //disable AIRS via open drains
+    car_started = 0; //revert car to cold start state to reinitiate precharge/RTDS 
+  }
+
+
   
 }
 
+
 void transmit() {
-  Wire.beginTransmission(WHEELADDR); // transmit to device #8
+  Wire.beginTransmission(WHEELADDR); // transmit to device @ address 0x08
   
   //Wire.write(rpm);
   //Wire.write(odo);
@@ -66,20 +84,46 @@ void transmit() {
   Wire.write(BMS_FLT);
   Wire.write(SEVCON_FLT);
   Wire.write(BRAKE_FLT);
-  
+
   Wire.endTransmission();    
   //Stop transmitting
 }
 
 void readIn() {
   
-  uint8_t IMD_FLT = digitalRead(IMD_FLT_IN);
-  uint8_t BMS_FLT = digitalRead(BMS_FLT_IN);
-  uint8_t SEVCON_FLT = digitalRead(IMD_FLT_IN);
-  uint8_t BRAKE_FLT = digitalRead(BOTS_IN);
-  uint8_t TMSM_FLT = digitalRead(TSMS_IN);
+  IMD_FLT = digitalRead(IMD_FLT_IN);
+  BMS_FLT = digitalRead(BMS_FLT_IN);
+  SEVCON_FLT = digitalRead(SEVCON_FLT_IN);
+  BRAKE_FLT = digitalRead(BOTS_IN);
+  TSMS_FLT = digitalRead(TSMS_IN);
+  COCKPIT_SW = digitalRead(COCKPIT_SW_IN);
+  dir = getDirection();
   
-  uint8_t START_INPUT = digitalRead(START_INPUT_IN);
-  uint8_t COCKPIT_SW = digitalRead(COCKPIT_SW_IN);
+}
+
+void playRTDS(void){
+
+  digitalWrite(RTDS_OUT, HIGH);
+  delay(2000);
+  digitalWrite(RTDS_OUT, LOW);
+
+}
+
+int getDirection(){
+
+  uint8_t drive = digitalRead(DRIVE_IN);
+  uint8_t reverse = digitalRead(REVERSE_IN);
+
+  if(drive && reverse){
+    return 1;
+  }
+  else if(reverse){
+    return 0;
+  }
+  else{
+    return 2;
+  }
+
+
 }
 
--- mobo_code/pins.h
+++ mobo_code/pins.h
@@ -2,24 +2,28 @@
 #define PINS_H
 
 //
-#define RX_IN D0
-#define TX_OUT D1
+#define RX_IN 0
+#define TX_OUT 1
 
-#define COCKPIT_SW_IN D2
-#define TSMS_IN D3
-#define BOTS_IN D4
+#define COCKPIT_SW_IN 2
+#define TSMS_IN 3
+#define BOTS_IN 4
 
-#define CONT_REQ_OUT D5
+#define CONT_REQ_OUT 5
 
 //Faults
-#define IMD_FLT_IN D6
-#define BMS_FLT_IN D7
-
+#define IMD_FLT_IN 6
+#define BMS_FLT_IN 7
+#define SEVCON_FLT_IN 16
 //
-#define UGLOW_OUT D8
-#define RTDS_OUT D9
+#define UGLOW_OUT 8
+#define RTDS_OUT 9
 
 //Start input
-#define START_INPUT_IN D10
+#define START_INPUT_IN 10
+
+#define DRIVE_IN 14
+#define REVERSE_IN 15 //active low 
+
 
 #endif
