ad98956eaf06d042af242f892591da4623fd823e
noelhwang <noelhwang@gmail.com>
2017-04-21 01:35:34 -0400

    Fixed Pins for Motherboard Rev 2.0.3 Add direction read from steering wheel

--- mobo_code/mobo_code.ino
+++ mobo_code/mobo_code.ino
@@ -5,7 +5,7 @@ uint8_t rpm = 99;           //rpm
 uint8_t odo = 99;           //miles
 uint8_t battery_temp = 128; //Farenheit
 uint8_t t_pressure = 90;    //psi
-uint8_t dir = 1;            //0 = forward, 1 = neutral, 2 = reverse
+uint8_t dir = 0;            //1 = forward, 0 = neutral, -1 = reverse
 uint8_t spd = 10;           //speed in mph
 uint8_t LV_SOC = 5;         //Low Volatage State of Charge
 uint8_t HV_SOC = 7;         //Low Voltage State of Charge
@@ -15,10 +15,8 @@ uint8_t BMS_FLT = 0;        //BMS Fault state variable
 uint8_t SEVCON_FLT = 0;     //Sevcon Fault
 uint8_t BRAKE_FLT = 0;      //BOTS Fault
 uint8_t TSMS_FLT = 0;       //TSMS Open
-
 uint8_t COCKPIT_SW = 0;     //cockpit switch
 
-uint8_t uglow = 0;
 uint8_t car_started = 0;
 
 const uint8_t WHEELADDR = 8;
@@ -29,6 +27,25 @@ void playRTDS(void);
 
 void setup() {
   Wire.begin(); // join i2c bus as master
+  pinMode(SOC_IN, INPUT);
+  pinMode(WHEEL3_IN, INPUT);
+  pinMode(WHEEL2_IN, INPUT);
+  pinMode(WHEEL1_IN, INPUT);
+  pinMode(WHEEL0_IN, INPUT);
+  pinMode(START_INPUT_IN, INPUT);
+  pinMode(BMS_FLT_IN, INPUT);
+  pinMode(SEVCON_FLT_IN, INPUT);
+  pinMode(IMD_FLT_IN, INPUT);
+  pinMode(COCKPIT_SW_IN, INPUT);
+  pinMode(TSMS_IN, INPUT);
+
+  pinMode(CONT_REQ_OUT, OUTPUT);
+  pinMode(RTDS_OUT, OUTPUT);
+  pinMode(FORWARD_OUT, OUTPUT);
+  pinMode(REVERSE_OUT, OUTPUT);
+  
+ 
+  /*
   pinMode(START_INPUT_IN, INPUT);
   pinMode(CONT_REQ_OUT, OUTPUT);
   pinMode(DRIVE_IN, INPUT_PULLUP); //active low
@@ -39,35 +56,35 @@ void setup() {
   pinMode(BOTS_IN, INPUT);
 
   pinMode(RTDS_OUT, OUTPUT); //initialize RTDS 
+  */
+  
   Serial.begin(9600);
   Serial.println("Setup done");
 }
 
 void loop() {
 
-  
-
+ 
   readIn();
   transmit();
   //check to see if car requested to start
   if(car_started){
     //Serial.println("Started");
-    Serial.println(" ------- brake, cockpit switch --------");
-    Serial.println(digitalRead(BOTS_IN));
-    Serial.println(digitalRead(COCKPIT_SW_IN));
+    //Serial.println(" ------- brake, cockpit switch --------");
+    //Serial.println(digitalRead(BOTS_IN));
+    //Serial.println(digitalRead(COCKPIT_SW_IN));
   }else{
     Serial.println("Waiting for Start Input");
   }
   if (digitalRead(START_INPUT_IN) && car_started == 0) //for case when cold start i.e. cockpit switch not tripped
-  {
+  {   
       Serial.println("Start Input Received");    
       car_started = 1; //set flag that car has been started
       digitalWrite(CONT_REQ_OUT, HIGH); //set contact request high to close AIRs
       Serial.println("contact request on");
       delay(6000); //wait for precharge
-      while(digitalRead(DRIVE_IN) && digitalRead(REVERSE_IN)){ 
+      while(dir == 0){ 
         Serial.println("Waiting for Drive or Reverse");
-        
         readIn();
         transmit();
         // while no direction input, car is in neutral, wait for drive direction input. NOTE:DRIVE_IN and REVERSE_IN are active low
@@ -78,13 +95,11 @@ void loop() {
         
       playRTDS(1000);
   }
-  if(COCKPIT_SW && !BRAKE_FLT && car_started == 1){ //cockpit switch tripped by driver after car has been started
+  if(COCKPIT_SW && !BRAKE_FLT && car_started){ //cockpit switch tripped by driver after car has been started
     digitalWrite(CONT_REQ_OUT, LOW); //disable AIRS via open drains
     Serial.println("contact request off");
     car_started = 0; //revert car to cold start state to reinitiate precharge/RTDS 
   }
-
-
   
 }
 
@@ -111,14 +126,13 @@ void transmit() {
 }
 
 void readIn() {
-  
+  //add i2c read from steering wheel for drive direction
   IMD_FLT = digitalRead(IMD_FLT_IN);
   BMS_FLT = digitalRead(BMS_FLT_IN);
   SEVCON_FLT = !digitalRead(SEVCON_FLT_IN);
   BRAKE_FLT = digitalRead(BOTS_IN);
   TSMS_FLT = digitalRead(TSMS_IN);
   COCKPIT_SW = digitalRead(COCKPIT_SW_IN);
-  dir = getDirection();
   
 }
 
@@ -133,28 +147,6 @@ void playRTDS(uint8_t delay_time){
   
 }
 
-int getDirection(){
-
-  uint8_t drive = digitalRead(DRIVE_IN);
-  uint8_t reverse = digitalRead(REVERSE_IN);
-
-  if(drive && reverse){
-    return 1;
-  }
-  else if(reverse){
-    return 0;
-  }
-  else{
-    return 2;
-  }
 
 
-}
-
-void fault2Serial(){
-
-  Serial.println("");
-  
-  
-}
 
--- mobo_code/pins.h
+++ mobo_code/pins.h
@@ -2,28 +2,24 @@
 #define PINS_H
 
 //
-#define RX_IN 0
-#define TX_OUT 1
-
-#define COCKPIT_SW_IN 2
-#define TSMS_IN 3
-#define BOTS_IN 4
+#define SOC_IN A0
+#define WHEEL3_IN A1
+#define WHEEL2_IN A3
+#define WHEEL1_IN A4
+#define WHEEL0_IN A2
 
 #define CONT_REQ_OUT 5
+#define START_INPUT_IN 18
+#define BMS_FLT_IN 31
+#define SEVCON_FLT_IN 29
+#define BOTS_IN 27
+#define IMD_FLT_IN 25
+#define RTDS_OUT 23
+#define COCKPIT_SW_IN 28
+#define TSMS_IN 30
+#define FORWARD_OUT 50
+#define REVERSE_OUT 48
 
-//Faults
-#define IMD_FLT_IN 6
-#define BMS_FLT_IN 7
-#define SEVCON_FLT_IN 52
-//
-#define UGLOW_OUT 8
-#define RTDS_OUT 9
-
-//Start input
-#define START_INPUT_IN 10
-
-#define DRIVE_IN 14
-#define REVERSE_IN 12 //active low 
 
 
 #endif
