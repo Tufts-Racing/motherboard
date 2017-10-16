f9d9cb0746b49c87f52b003ee055e835038049f0
Zachary Faber Manning <zfabermanning@gmail.com>
2017-10-16 12:18:14 -0400

    Added design files 2018 with latest version of eagle files

--- mobo_code/mobo_code.ino
+++ mobo_code/mobo_code.ino
@@ -10,7 +10,7 @@ uint8_t spd = 10;           //speed in mph
 uint8_t LV_SOC = 5;         //Low Volatage State of Charge
 uint8_t HV_SOC = 7;         //Low Voltage State of Charge
 
-uint8_t IMD_FLT = 0;        //IMD Fault state variable
+uint8_t IMD_FLT = 1;        //IMD Fault state variable
 uint8_t BMS_FLT = 0;        //BMS Fault state variable
 uint8_t SEVCON_FLT = 0;     //Sevcon Fault
 uint8_t BRAKE_FLT = 0;      //BOTS Fault
@@ -39,7 +39,7 @@ void setup() {
   pinMode(IMD_FLT_IN, INPUT);
   pinMode(COCKPIT_SW_IN, INPUT);
   pinMode(TSMS_IN, INPUT);
-
+  pinMode(52,OUTPUT);
   pinMode(CONT_REQ_OUT, OUTPUT);
   pinMode(RTDS_OUT, OUTPUT);
   pinMode(FORWARD_OUT, OUTPUT);
@@ -70,6 +70,7 @@ void setup() {
 }
 int i = 0;
 void loop() {
+  
   i++;
   if(i>500){
     Serial.println("----------------------------------------");
@@ -79,6 +80,8 @@ void loop() {
     Serial.println(digitalRead(BOTS_IN));
     Serial.println("TSMS");
     Serial.println(digitalRead(TSMS_IN));
+    Serial.println("IMD");
+    Serial.println(digitalRead(IMD_FLT_IN));
     i=0;
   }
   readIn();
@@ -100,23 +103,37 @@ void loop() {
       Serial.println("contact request on");
       delay(6000); //wait for precharge
       while(dir == 0){ 
+        //while no direction input, car is in neutral, wait for drive direction input. NOTE:DRIVE_IN and REVERSE_IN are active low
+        //spin until forward or reverese is selected
         Serial.println("Waiting for Drive or Reverse");
         readIn();
         transmit();
-        // while no direction input, car is in neutral, wait for drive direction input. NOTE:DRIVE_IN and REVERSE_IN are active low
-      //spin until forward or reverese is selected
+        if(!COCKPIT_SW && BRAKE_FLT && car_started){ //cockpit switch tripped by driver after car has been started
+          digitalWrite(CONT_REQ_OUT, LOW); //disable AIRS via open drains
+          Serial.println("contact request off");
+          car_started = 0; //revert car to cold start state to reinitiate precharge/RTDS 
+          break;
+        }
+        if(!BRAKE_FLT){ //cockpit switch tripped by driver after car has been started
+          digitalWrite(CONT_REQ_OUT, LOW); //disable AIRS via open drains
+          Serial.println("contact request off");
+        }
       }
       readIn();
       transmit();
-        
-      playRTDS(1000);
+      if(car_started&&BRAKE_FLT){
+        playRTDS(1000);
+      }
   }
   if(!COCKPIT_SW && BRAKE_FLT && car_started){ //cockpit switch tripped by driver after car has been started
     digitalWrite(CONT_REQ_OUT, LOW); //disable AIRS via open drains
     Serial.println("contact request off");
     car_started = 0; //revert car to cold start state to reinitiate precharge/RTDS 
   }
-  
+  if(!BRAKE_FLT){ //cockpit switch tripped by driver after car has been started
+    digitalWrite(CONT_REQ_OUT, LOW); //disable AIRS via open drains
+    Serial.println("contact request off");
+  }
 }
 
 
@@ -135,7 +152,7 @@ void transmit() {
   Wire.write(BMS_FLT);
   Wire.write(SEVCON_FLT);
   Wire.write(BRAKE_FLT);
-
+  Wire.write(COCKPIT_SW);
   Wire.endTransmission(true);    
   //Stop transmitting
 }
@@ -148,7 +165,8 @@ void readIn() {
      dir = Wire.read();
   }
   
-  IMD_FLT = digitalRead(IMD_FLT_IN);
+  IMD_FLT = !digitalRead(IMD_FLT_IN);
+  digitalWrite(52,IMD_FLT);
   BMS_FLT = digitalRead(BMS_FLT_IN);
   SEVCON_FLT = !digitalRead(SEVCON_FLT_IN);
   BRAKE_FLT = digitalRead(BOTS_IN);
